6a1e0d3e14c5bb5780f78af824fc5c4e67a036d5
aaronh1986 <aaronharbourne@gmail.com>
2021-04-20 16:19:45 +0000

    Add more info from api

--- script.js
+++ script.js
@@ -44,6 +44,9 @@ fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id
 .then(response => response.json()) //This promise returns response in JSON format
 .then((data) => showEvents(data));    /* sortInfo(data) */
 
+function myWay(json) {
+    
+}
 
 function showEvents(json) {
   var items = $('#events .list-group-item');
@@ -54,7 +57,10 @@ function showEvents(json) {
   for (var i=0;i<events.length;i++) {
     item.children('.list-group-item-heading').text(events[i].name);
     item.children('.list-group-item-text').text(events[i].dates.start.localDate);
-   
+    item.children('.status').text(events[i].dates.status.code);
+    item.children('.promoter').text(events[i].promoters[1].name);
+    item.children('.note').text(events[i].pleaseNote)
+  /*  item.children('.cost').text(events)[i].priceRanges[0].max ;  */
     try {
       item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
     } catch (err) {
@@ -72,15 +78,15 @@ function showEvents(json) {
     });
     item=item.next();
   }
+  console.log(events)
 }
 
 
-function getAttraction(id) {
+ function getAttraction(id) {
     fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=deftones&id=16vOZpfQ9G7SOsV&apikey=6UVAl4kH2qy1pxo2XkYN6ZAIgLJIX3SK")
     .then(response => response.json()) //This promise returns response in JSON format
     .then((data) => showEvents(data));
 }
-
 function showAttraction(json) {
   $('#events-panel').hide();
   $('#attraction-panel').show();
@@ -94,4 +100,4 @@ function showAttraction(json) {
   $('#classification').text(json.classifications[0].segment.name + " - " + json.classifications[0].genre.name + " - " + json.classifications[0].subGenre.name);
 }
 
-getEvents(page);
+getEvents(page); 
